56d75dfc494cc2e5241f49b5c0e85cdac6bfc5cf
Helen Liang <h.liang93@gmail.com>
2014-10-23 20:22:18 -0700

    Comment out get for profile rout

--- pwnedapp/pwnedapp/routes/catchall.js
+++ pwnedapp/pwnedapp/routes/catchall.js
@@ -48,11 +48,11 @@ module.exports = function (app, passport) {
     // =====================================
     // we will want this protected so you have to be logged in to visit
     // we will use route middleware to verify this (the isLoggedIn function)
-    app.get('/profile', common.isLoggedIn, function (req, res) {
-        res.render('profile.ejs', {
-            user : req.user // get the user out of session and pass to template
-        });
-    });
+//    app.get('/profile', common.isLoggedIn, function (req, res) {
+//        res.render('profile.ejs', {
+//            user : req.user // get the user out of session and pass to template
+//        });
+//    });
     
     // =====================================
     // LOGOUT ==============================
