c63225731922c9231fcca6e7e6b812abdbf00dc3
TREVAND174 <trevoranderson17@gmail.com>
2014-10-31 12:29:41 -0700

    Added a sample product schema/endpoint

---
+++ pwnedapp/pwnedapp/models/product.js
@@ -0,0 +1,15 @@
+﻿var mongoose = require('mongoose');
+
+// define the schema for our user model
+var productSchema = mongoose.Schema({
+    name: String,
+    price: Number,
+    imageurl: String,
+    scraperParams: {
+        site: String,
+        lastAccess: Date,
+    }
+});
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('Product', productSchema);
--- pwnedapp/pwnedapp/routes/catchall.js
+++ pwnedapp/pwnedapp/routes/catchall.js
@@ -1,4 +1,5 @@
 ﻿var common = require('../common/common.js');
+var productDB = require('../models/product.js');
 // app/routes.js
 module.exports = function (app, passport) {
     
@@ -8,7 +9,28 @@ module.exports = function (app, passport) {
     app.get('/', function (req, res) {
         res.render('../index.html'); // load the index.ejs file
     });
-    
+    app.get('/products', function (req, res) {
+        productDB.find().exec(function (err, products) {
+            // Quick hack to put *something* in the database if nothing is there. Remove when proper CRUD is enabled
+            if (products.length === 0) {
+                var p1 = new productDB();
+                var p2 = new productDB();
+
+                p1.name = "Ebola Spray";
+                p1.price = 9.99;
+                p1.imageurl = "http://i.imgur.com/uJg4bh2.jpg";
+                p1.lastAccess = new Date();
+                p1.save();
+
+                p2.name = "Lysol";
+                p2.price = 23.98;
+                p2.imageurl = "/images/exampleimage.jpg";
+                p2.lastAccess = new Date();
+                p2.save();
+            }
+            res.json(products);
+        });
+    });
     // =====================================
     // LOGIN ===============================
     // =====================================
